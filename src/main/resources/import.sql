22576589ce50acf829ef96fe969cd369f2efbc8b
Jorge Meza Bello <jorberiver@gmail.com>
2022-12-02 00:37:18 -0600

    Creacion del import sql!

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO productos (nombre, precio, created_at) VALUES ('Grabadora Panasonic', 1680, NOW()); 
+INSERT INTO productos (nombre, precio, created_at) VALUES ('Televisor Samsung', 5000, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES ('Horno Eléctrico LG', 2500, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES ('Grabadora Panasonic', 1680, NOW());   
