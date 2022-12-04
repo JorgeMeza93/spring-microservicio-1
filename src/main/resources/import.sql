ac4fb161f02d9ad1c0533db97df8653068066ed5
Jorge Meza Bello <jorberiver@gmail.com>
2022-12-03 18:57:49 -0600

    Cambios en el import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,5 @@
 INSERT INTO productos (nombre, precio, created_at) VALUES ('Grabadora Panasonic', 1680, NOW()); 
 INSERT INTO productos (nombre, precio, created_at) VALUES ('Televisor Samsung', 5000, NOW());
 INSERT INTO productos (nombre, precio, created_at) VALUES ('Horno Eléctrico LG', 2500, NOW());
-INSERT INTO productos (nombre, precio, created_at) VALUES ('Grabadora Panasonic', 1680, NOW());   
+INSERT INTO productos (nombre, precio, created_at) VALUES ('Laptop Asus', 12700, NOW());   
+INSERT INTO productos (nombre, precio, created_at) VALUES ('Bocina Inalambrica Bose', 5000, NOW()); 
